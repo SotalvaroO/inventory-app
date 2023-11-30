780634724e6eb4ec3e3ebbbfd8d61b75571aedab
Santiago Otálvaro <sotalvaroo1999@gmail.com>
2023-11-30 14:22:40 -0500

    detail added

--- app/infrastructure/schemas.py
+++ app/infrastructure/schemas.py
@@ -1,4 +1,4 @@
-def individual_serial(product) -> dict:
+def individual_serial_product(product) -> dict:
     return {
         "product_sku" : product["sku"],
         "name" : product["name"],
@@ -8,5 +8,16 @@ def individual_serial(product) -> dict:
         "thumbnail" : product["thumbnail"],
     }
     
-    def list_serial(products) -> list:
-        return [individual_serial(product) for product in products]
+def list_serial_product(products) -> list:
+    return [individual_serial_product(product) for product in products]
+
+def individual_serial_product_detail(product_detail) -> dict:
+    return {
+        "product_sku" : product_detail["sku"],
+        "name" : product_detail["name"],
+        "description" : product_detail["description"],
+        "price" : product_detail["price"],
+        "stock" : product_detail["stock"],
+        "images" : product_detail["images"],
+    }
+    
